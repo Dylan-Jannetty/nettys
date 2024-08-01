40282371eccf0e0165a12456123c86f72d7bab6c
Dylan Jannetty <dylanjannetty@dylans-mbp.mynetworksettings.com>
2024-08-01 13:32:53 -0400

    add compressed image package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import compress from "astro-compress";
+
+import betterImageService from "astro-better-image-service";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress()]
+  integrations: [compress(), betterImageService()]
 });
