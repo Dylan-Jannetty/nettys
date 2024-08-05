7bcadbb5053a370709888498ce544119ad4a659a
Dylan Jannetty <dylanjannetty@dylans-mbp.mynetworksettings.com>
2024-08-05 12:40:52 -0400

    remove astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import compress from "astro-compress";
 
 import betterImageService from "astro-better-image-service";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), betterImageService()]
+  integrations: [betterImageService()]
 });
